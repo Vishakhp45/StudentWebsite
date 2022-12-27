78060307ef12881d3a886843f70b58b551d7d54e
user <user@DESKTOP-5LN8OOV>
2022-12-27 04:26:49 -0500

    Add project files.

---
+++ Helper/HelperClass.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace StudentWebsite.Helper
+{
+    public class HelperClass
+    {
+        public void WriteaFile(string name,int cls,int roll)
+        {
+            var path = @"C:\Users\user\source\repos\StudentWebsite\Notepad\A.txt";
+            StreamWriter ob = new StreamWriter(path, true);
+
+
+            ob.WriteLine("Name     :"+name);
+            ob.WriteLine("Class    :"+cls);
+            ob.WriteLine("RollNo   :"+roll);
+
+            ob.Close();
+            Process.Start("notepad.exe", path );
+
+        }
+    }
+}
